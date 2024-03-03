5fc5daabaf9bb63750617188f11986ef8b959b91
MoeDevelops <niesenmoritz@gmail.com>
2024-03-03 18:51:42 +0100

    Added check for length

--- bot.py
+++ bot.py
@@ -17,5 +17,9 @@ async def on_ready():
 @tree.command(name="rename", description="Renames a user")
 async def rename(interaction: Interaction, user: Member, new_name: str):
     print(f"Renaming '{user.nick}' ('{user.name}') to '{new_name}'")
+
+    if (len(new_name) > 32):
+        await interaction.response.send_message("New name is too long")
+
     await user.edit(nick=new_name)
     await interaction.response.send_message("User renamed successfully")
