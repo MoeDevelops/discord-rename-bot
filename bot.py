53352f2350bce445a5de7a608ca1fb4d2502ce1c
MoeDevelops <niesenmoritz@gmail.com>
2024-03-03 19:03:23 +0100

    Return after invalid name

--- bot.py
+++ bot.py
@@ -20,6 +20,7 @@ async def rename(interaction: Interaction, user: Member, new_name: str):
 
     if (len(new_name) > 32):
         await interaction.response.send_message("New name is too long")
+        return
 
     await user.edit(nick=new_name)
     await interaction.response.send_message("User renamed successfully")
